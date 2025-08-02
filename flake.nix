0e28e9c9850b3159f147530baaa042ac2fab6729
comradeshr00m <typedfrog@proton.me>
2025-08-02 19:51:01 +0100

    build: export overlay instead of home manager modules

--- flake.nix
+++ flake.nix
@@ -27,63 +27,38 @@
         # https://github.com/NixOS/nixpkgs/blob/e456032addae76701eb17e6c03fc515fd78ad74f/flake.nix#L76
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        rustToolChain = pkgs.rust-bin.nightly.latest.default.override {
-          extensions = [
-            "rust-src"
-            "rust-analyzer"
-          ];
-        };
         treeConfig =
           { ... }:
           {
-            programs.nixfmt-rfc-style.enable = true;
+            programs.nixfmt.enable = true;
           };
         treefmtEval = treefmt-nix.lib.evalModule pkgs treeConfig;
-
         baanPkgs = pkgs.callPackage ./package.nix { };
       in
       {
 
-        packages.default = baanPkgs.baan;
-        packages.baan = baanPkgs.baan;
-        lib.makeBaanWrapper = baanPkgs.makeBaanWrapper;
-
-        # === DEV SHELL ===
-
-        # https://github.com/hsjobeki/nixpkgs/blob/migrate-doc-comments/pkgs/build-support/mkshell/default.nix#L9:C1
-        devShells.default =
-          with pkgs;
-          mkShell {
-            # Concats with nativeBuildInputs
-            nativeBuildInputs = [
-              lld
-              # cargo flame-graph
-              rustToolChain
-            ] ++ (lib.optional pkgs.stdenv.isDarwin darwin.apple_sdk.frameworks.CoreFoundation);
-            buildInputs = [ pkg-config ];
-            env = {
-              # Set environment variables to help tools find rust-src
-              RUST_SRC_PATH = "${rustToolChain}/lib/rustlib/src/library";
-              # Development
-              BAAN_HOME_DIR = "./target/notes";
-            };
-          };
-
-        formatter = treefmtEval.config.build.wrapper;
         checks = {
           formatting = treefmtEval.config.build.check self;
-
         };
-
+        devShells.default = import ./shell.nix {
+          inherit (pkgs)
+            darwin
+            lib
+            lld
+            mkShell
+            pkg-config
+            rust-bin
+            stdenv
+            ;
+        };
+        formatter = treefmtEval.config.build.wrapper;
+        packages.default = baanPkgs.baan;
+        packages.baan = baanPkgs.baan;
       }
     )
-    // (
-      let
-        homeManagerModule = import ./home-manager.nix;
-      in
-      {
-        nixosModules.default = homeManagerModule;
-        nixosModules.homeManagerModules.baan = homeManagerModule;
-      }
-    );
+    // {
+      overlays.default = final: prev: {
+        baan = (prev.callPackage ./package.nix { }).baan;
+      };
+    };
 }
---
+++ shell.nix
@@ -0,0 +1,34 @@
+# Ref:
+# https://github.com/hsjobeki/nixpkgs/blob/migrate-doc-comments/pkgs/build-support/mkshell/default.nix#L9:C1
+{
+  darwin,
+  lib,
+  lld,
+  mkShell,
+  pkg-config,
+  rust-bin,
+  stdenv,
+}:
+let
+  rustToolChain = rust-bin.nightly.latest.default.override {
+    extensions = [
+      "rust-src"
+      "rust-analyzer"
+    ];
+  };
+in
+mkShell {
+  # Concats with nativeBuildInputs
+  nativeBuildInputs = [
+    lld
+    # cargo flame-graph
+    rustToolChain
+  ] ++ (lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.CoreFoundation);
+  buildInputs = [ pkg-config ];
+  env = {
+    # Set environment variables to help tools find rust-src
+    RUST_SRC_PATH = "${rustToolChain}/lib/rustlib/src/library";
+    # Development
+    #BAAN_HOME_DIR = "./target/notes";
+  };
+}
